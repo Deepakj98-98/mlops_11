9733f0d39a9b6019d1b83172499e94f98961613c
Deepakj98-98 <deepak10.bh@gmail.com>
2024-07-29 07:45:35 +0530

    end commit

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
 import joblib
+
 housing = pd.read_csv('housing.csv')
 
 x_train, x_test, y_train, y_test = train_test_split(housing.median_income, housing.median_house_value, test_size = 0.2)
